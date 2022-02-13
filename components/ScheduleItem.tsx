48a2b90c792057461bcfd61840e679ad03f8f619
Nick Young <youngnicholas228@gmail.com>
2022-02-12 19:37:39 -0500

    handouts

--- components/ScheduleItem.tsx
+++ components/ScheduleItem.tsx
@@ -1,8 +1,11 @@
 export default function ScheduleItem({
                                          name,
                                          description,
-                                         image
-                                     }: { name: string; description: string; image: string; }) {
+                                         image,
+                                         handout,
+                                         slides,
+                                         recording,
+                                     }: { name: string; description: string; image: string; handout: string; slides: string; recording: string; }) {
     return <li
         className="p-5 md:p-6 backdrop-blur-md bg-opacity-50 border border-[#30363C] bg-[#0B1016] rounded-lg">
         <div className="flex items-center justify-start">
@@ -16,33 +19,30 @@ export default function ScheduleItem({
                 </p>
             </div>
         </div>
-        {/*<div className="space-x-5 mt-6 text-xs md:text-sm opacity-50">*/}
-        {/*    <a className="inline-flex items-center">*/}
-        {/*        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20"*/}
-        {/*             fill="currentColor">*/}
-        {/*            <path fillRule="evenodd"*/}
-        {/*                  d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z"*/}
-        {/*                  clipRule="evenodd"/>*/}
-        {/*        </svg>*/}
-        {/*        Handout*/}
-        {/*    </a>*/}
-        {/*    <a className="inline-flex items-center">*/}
-        {/*        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20"*/}
-        {/*             fill="currentColor">*/}
-        {/*            <path*/}
-        {/*                d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"/>*/}
-        {/*        </svg>*/}
-        {/*        Slides*/}
-        {/*    </a>*/}
-        {/*    <a className="inline-flex items-center">*/}
-        {/*        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20"*/}
-        {/*             fill="currentColor">*/}
-        {/*            <path fillRule="evenodd"*/}
-        {/*                  d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"*/}
-        {/*                  clipRule="evenodd"/>*/}
-        {/*        </svg>*/}
-        {/*        Watch Recording*/}
-        {/*    </a>*/}
-        {/*</div>*/}
+        <div className="space-x-5 mt-6 text-xs md:text-sm opacity-50">
+           <a className="inline-flex items-center" href={handout}>
+               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20">
+                    <path fillRule="evenodd" fill="currentColor" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z" clipRule="evenodd"/>
+                </svg>
+                Handout
+            </a>
+            {slides && <a className="inline-flex items-center" href={slides}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20"
+                     fill="currentColor">
+                    <path
+                        d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"/>
+                </svg>
+                Slides
+            </a>}
+            {recording && <a className="inline-flex items-center" href={recording}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20"
+                     fill="currentColor">
+                    <path fillRule="evenodd"
+                          d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+                          clipRule="evenodd"/>
+                </svg>
+                Watch Recording
+            </a>}
+        </div>
     </li>
 }
